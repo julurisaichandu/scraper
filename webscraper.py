fde697be242d029d40a3b355da1507e0992fd039
Saichandu Juluri <124517156+julurisaichandu@users.noreply.github.com>
2024-10-28 12:12:48 -0400

    Update webscraper.py

--- webscraper.py
+++ webscraper.py
@@ -1,4 +1,4 @@
-import undetected_chromedriver as uc
+# import undetected_chromedriver as uc
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
